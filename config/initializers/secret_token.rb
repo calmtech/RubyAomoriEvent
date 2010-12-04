4796324b02ea60fc09f8f95ade5ba5f10eb76bde
calmtech <mkogawa34@gmail.com>
2010-12-04 15:56:10 +0900

    Rails3で初期ファイルを作成

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RubyAomoriEvent::Application.config.secret_token = '486bf0ecdaaaa3c1a02ea44f5bcda74c134c67c2af200dd9afedfa1cc4119e2c7fa6af46c1c8a82f57ea8c5f370eab30a3866161f96f88e55d1655de6c98a860'
